867c9c6b6253dfa2fe311f643ee5964253d940d0
aadilghani1 <aadilghani1@gmail.com>
2021-04-09 13:40:59 +0530

    First Build

---
+++ src/Components/BuyPage.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import CartItem from "./CartItem";
+
+import { random, commerce } from "faker";
+import { Container, Col, Row } from "reactstrap";
+
+const apiKey = "563492ad6f917000010000018fd0d65bea78481f95987d20d7903cbe";
+
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+const localurl = "https://api.myjson.com/bins/qp9uo";
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
+
+  //   const fetchPhotos = async () => {
+  //     const response = await Axios.get(url, {
+  //       header: {
+  //         Authorization: apiKey
+  //       }
+  //     });
+
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(url, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+
+    const { photos } = data;
+
+    const allProduct = photos.map((photo) => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map((product) => (
+          <Col md={4} key={product.id}>
+            <CartItem product={product} addInCart={addInCart} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default BuyPage;
